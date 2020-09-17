ed0c202fac6d1b52892b256fae2e62ceae382657
Cherie Cookson <cheriecookson@gmail.com>
2020-09-16 22:34:03 -0500

    added user and updated thoughts

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,24 +1,10 @@
 const router = require('express').Router();
+const { addThought, removeThought } = require('../../controllers/thought-controller');
 
-const {
-    getAllThought,
-    getThoughtById,
-    createThought,
-    updateThought,
-    deleteThought
-} = require('../../controllers/thought-controller');
+// /api/thoughts/<userId>
+router.route('/:userId').post(addThought);
 
-// Set up GET all and POST at /api/thoughts
-router
-  .route('/')
-  .get(getAllThought)
-  .post(createThought);
-
-// Set up GET one, PUT, and DELETE at /api/thoughts/:id
-router
-  .route('/:id')
-  .get(getThoughtById)
-  .put(updateThought)
-  .delete(deleteThought);
+// /api/thoughts/<userId>/<thoughtId>
+router.route('/:userId/:thoughtId').delete(removeThought);
 
 module.exports = router;
